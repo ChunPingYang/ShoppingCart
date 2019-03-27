30e1c6ca857df0df1db7988a127841ebdc229a21
Xi Cui <xicui521@gmail.com>
2019-03-26 21:53:36 -0500

    remove css

--- regipage.php
+++ regipage.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type = "text/css" href = "regi_style.css">
+<link rel="stylesheet" type = "text/css" href = "css/regi_style.css">
 
 </head>
 <body>
