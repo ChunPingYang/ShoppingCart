be388ed75709106bd1fe9f72b188ed767e17e53b
CHUNPINGYANG <cxy180010@utdallas.edu>
2019-04-27 11:45:03 -0500

    Jason Update form submit

--- productList.php
+++ productList.php
@@ -7,61 +7,52 @@
 
 <?php
 
-	$price		= ( isset( $_GET['price'] ) ) ? $_GET['price'] : -1;
-	$option     = ( isset( $_GET['option'] ) ) ? $_GET['option'] : 1;
-	$limit      = ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
-	$page       = ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
-	$links      = ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
+	$price			= ( isset( $_GET['price'] ) ) ? $_GET['price'] : -1;
+	$option     	= ( isset( $_GET['option'] ) ) ? $_GET['option'] : 1;
+	$limit      	= ( isset( $_GET['limit'] ) ) ? $_GET['limit'] : 10;
+	$page       	= ( isset( $_GET['page'] ) ) ? $_GET['page'] : 1;
+	$links      	= ( isset( $_GET['links'] ) ) ? $_GET['links'] : 7;
+	$minimum_rating = ( isset( $_GET['minimum_rating'] ) ) ? $_GET['minimum_rating'] : 0;
+	$maximum_rating = ( isset( $_GET['maximum_rating'] ) ) ? $_GET['maximum_rating'] : 99; //TODO SQL Type to Integer
 	
-$sql = "";
+$sql = "SELECT * FROM product WHERE display = 1";
 if(isset($_POST['search'])){
-	$keyword = trim($_POST['search']);
 
-	$sql = "SELECT * FROM product WHERE pname like '%$keyword%' AND display = 1 ORDER BY price ASC";
-	if($option == 2){
-		$sql = "SELECT * FROM product WHERE pname like '%$keyword%' AND display = 1 ORDER BY price DESC";
-	}
+	$keyword = trim($_POST['search']);
+	$sql .= " AND pname like '%$keyword%'";
 
 }else{
 //10x+11 10x+20
-	if($price == -1){
-		$sql = "SELECT * FROM product WHERE display = 1 ORDER BY price ASC";
-		if($option == 2){
-			$sql = "SELECT * FROM product WHERE display = 1 ORDER BY price DESC";
-		}
-	}else if($price == 0){
-		$sql = "SELECT * FROM product WHERE price <= 20 AND display = 1 ORDER BY price ASC";
-		if($option == 2){
-			$sql = "SELECT * FROM product WHERE price <= 20 AND display = 1 ORDER BY price DESC";
+	if($price == 0){
+		$sql .= " AND price <= 20";
+		if(!empty($minimum_rating) && !empty($maximum_rating))
+		{
+			$sql .= " 
+				AND rating BETWEEN '".$minimum_rating."' AND '".$maximum_rating."'
+			";
 		}
 	}
 
 	for($i=1;$i<=3;$i++){
 		if($price == $i){
-			$sql = "SELECT * FROM product WHERE price BETWEEN '$i'*10+11 AND '$i'*10+20 AND display = 1 ORDER BY price ASC";
-			if($option == 2){
-				$sql = "SELECT * FROM product WHERE price BETWEEN '$i'*10+11 AND '$i'*10+20 AND display = 1 ORDER BY price DESC";
-			}
+			$sql .= " AND price BETWEEN $i*10+11 AND $i*10+20";
 			break;
 		}
 	}
 	
 	if($price == 4){
-		$sql = "SELECT * FROM product WHERE price >= 51 AND display = 1 AND display = 1 ORDER BY price ASC";
-		if($option == 2){
-			$sql = "SELECT * FROM product WHERE price >= 51 AND display = 1 AND display = 1 ORDER BY price DESC";
-		}
+		$sql .= " AND price >= 51";
 	}
+}
 
+$sql .= " 
+		AND rating BETWEEN '".$minimum_rating."%' AND '".$maximum_rating."%'
+		";
 
-
-	if($price == -1){
-		$sql = "SELECT * FROM product WHERE display = 1 ORDER BY price ASC";
-		if($option == 2){
-			$sql = "SELECT * FROM product WHERE display = 1 ORDER BY price DESC";
-		}
-	}
-
+if($option == 1){
+	$sql .= " ORDER BY price ASC";
+}else if($option == 2){
+	$sql .= " ORDER BY price DESC";
 }
 
  echo "sql: ".$sql;
@@ -78,27 +69,58 @@ if(isset($_POST['search'])){
 	$(document).ready(function(){
 
 		//Filter by price
-		$('input[type=radio][name=price]').change(function() {
-			console.log("price: "+this.value);  //TODO why this.value undefined
-			var price = this.value;
-			$.ajax({
-				type: "GET",
-				success: function(data){
-					window.location = '?option=<?php echo $option?>&price='+price;
-				},
-				error: function (xhr, ajaxOptions, thrownError) {
-					console.log("status: "+xhr.status);
-					console.log("Error: "+thrownError);
-				}
-			});
+		// $('input[type=radio][name=price]').change(function() {
+		// 	console.log("price: "+this.value);  //TODO why this.value undefined
+		// 	var price = this.value;
+		// 	$.ajax({
+		// 		type: "GET",
+		// 		success: function(data){
+		// 			window.location = '?option=<?php echo $option?>&price='+price;
+		// 		},
+		// 		error: function (xhr, ajaxOptions, thrownError) {
+		// 			console.log("status: "+xhr.status);
+		// 			console.log("Error: "+thrownError);
+		// 		}
+		// 	});
+		// });
+		$('input[type=radio][name=price]').click(function(){
+			$('#navForm').submit();
 		});
+
+		$('#rating_show').html(<?php echo $minimum_rating?> + ' - ' + <?php echo $maximum_rating?>);
+		$('#rating_range').slider({
+			range:true,
+			min:0,
+			max:99, //TODO SQL Type to Integer
+			values:[<?php echo $minimum_rating?>, <?php echo $maximum_rating?>],
+			step:10,
+			stop:function(event, ui)
+			{
+				$('#rating_show').html(ui.values[0] + ' - ' + ui.values[1]);
+				$('#hidden_minimum_rating').val(ui.values[0]);
+				$('#hidden_maximum_rating').val(ui.values[1]);
+				// $.ajax({
+				// 	type: "GET",
+				// 	success: function(data){
+				// 		window.location = '?option=<?php echo $option?>&price=<?php echo $price?>'
+				// 								+'&minimum_rating='+ui.values[0]+'&maximum_rating='+ui.values[1];
+				// 	},
+				// 	error: function (xhr, ajaxOptions, thrownError) {
+				// 		console.log("status: "+xhr.status);
+				// 		console.log("Error: "+thrownError);
+				// 	}
+				// });
+				document.getElementById("navForm").submit();
+
+			}
+    	});
 
 		$('input[type=radio][name=price]').each(function(){
 			if(this.value == <?php echo $price?>){
 				this.checked = true;
 			}
 		})
-
+		
 		
 	});
 </script>
@@ -106,23 +128,13 @@ if(isset($_POST['search'])){
 		<main class="card-container"> 
 				<nav class="sortbar">
 						<ul>
-							<li >Sort by:</li>
+							<li ><h2>Sort by:</h2></li>
 							<li>
 								<div name="sort" class="custom-select" style="width:150px;">
 									<select>
 										<option value="0">Top Rated</option>
 										<option value="1">Price:Low to High</option>
 										<option value="2">Price:High to Low</option>
-									</select>
-								</div>
-							</li>
-							<li>Display:</li>
-							<li>
-								<div id="viewPage" class="" style="width:150px;">
-									<select>
-										<option value="0">5 per page</option>
-										<option value="1">10 per page</option>
-										<option value="2">15 per page</option>
 									</select>
 								</div>
 							</li>
@@ -139,11 +151,13 @@ if(isset($_POST['search'])){
 									</div>
 									<div class="productDes">
 										<h2><?php echo $results->data[$i]['pname']; ?></h2>
-										<p><?php echo $results->data[$i]['description']; ?></p>
+										<i class="fa" style="font-size:48px;color:red">
+											<?php echo $results->data[$i]['rating']; ?>
+										</i>
 									</div>
 									<div class="col3">
 										<div class="column">
-
+											
 										</div>
 										<div class="column">
 											<i name="price" class="fa fa-dollar" style="font-size:48px;color:red">
@@ -163,5 +177,5 @@ if(isset($_POST['search'])){
 		
 		<?php include_once 'inc/footer.php';?>
 		
-		<script src="./js/productList.js?version=2.0"></script>
+		<script src="./js/productList.js?version=3.0"></script>
 		
